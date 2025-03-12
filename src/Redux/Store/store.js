7280a3fa66c27b696d36d9fdc89fa4ff3eeef9e5
Suhail <suhail518650@gmail.com>
2025-03-12 16:40:41 +0530

    voucher Request merged

--- src/Redux/Store/store.js
+++ src/Redux/Store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../Slices/AuthSlice";
 import institutionReducer from "../Slices/InstitutionSlice";
+import voucherReducer from "../Slices/VoucherSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     institution: institutionReducer,
+    Voucher: voucherReducer
   },
 });
 
